d376063f08b58431430e19e5702e3986e9712f33
Pere <pere.solaclaver@gmail.com>
2019-11-26 12:29:56 +0000

    Add tile bar to the grid tiles

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // PROJECT DATA
 import ProjectsData from "../database/Projects";
 
 // MATERIAL UI
 import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
+import GridListTileBar from "@material-ui/core/GridListTileBar";
+import IconButton from "@material-ui/core/IconButton";
+import PageviewIcon from "@material-ui/icons/Pageview";
 
 const useStyles = makeStyles(theme => ({
   root: {
-      display: "flex",
-      flexWrap: "wrap",
-      justifyContent: "space-around",
-      overflow: "hidden",
-      padding: '50px',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+    overflow: "hidden",
+    padding: "50px",
     //   background: "brown",
     backgroundColor: theme.palette.background.paper
   },
   gridList: {
-    width: "100%"
+    width: "100%",
+    transform: "translateZ(0)"
+  },
+  titleBar: {
+    background:
+      "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+      "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)"
+  },
+  icon: {
+    color: "white"
   }
 }));
 
@@ -28,7 +40,12 @@ export default function HomePage() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <GridList cellHeight={300} className={classes.gridList} cols={3} spacing={50}>
+      <GridList
+        cellHeight={300}
+        className={classes.gridList}
+        cols={3}
+        spacing={50}
+      >
         {ProjectsData.map(project => (
           <GridListTile
             key={project.id}
@@ -36,6 +53,23 @@ export default function HomePage() {
             rows={project.rows || 1}
           >
             <img src={project.img} alt={project.title} />
+            <GridListTileBar
+              title={project.title}
+              titlePosition="top"
+              actionIcon={
+                <IconButton
+                  aria-label={`star ${project.title}`}
+                  className={classes.icon}
+                  size={'medium'}
+                >
+                  <Link to={`/project/${project.id}`}>
+                    <PageviewIcon  disabled={true}/>
+                  </Link>
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
           </GridListTile>
         ))}
       </GridList>
--- src/database/Projects.js
+++ src/database/Projects.js
@@ -21,7 +21,7 @@ const ProjectData = [
       "https://images.unsplash.com/photo-1560518700-7a5559afb10c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=925&q=80",
     title: "Tour Eiffel Street",
     cols: 2,
-    rows: 1
+    rows: 2
   },
   {
     id: 3,
@@ -29,7 +29,7 @@ const ProjectData = [
       "https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     title: "Tour Eiffel Street",
     cols: 1,
-    rows: 1
+    rows: 2
   },
   {
     id: 5,
@@ -37,7 +37,7 @@ const ProjectData = [
       "https://images.unsplash.com/photo-1509041322357-8a3f9757a475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80",
     title: "Tour Eiffel Street",
     cols: 1,
-    rows: 1
+    rows: 2
   },
   {
     id: 6,
@@ -45,7 +45,7 @@ const ProjectData = [
       "https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
     title: "Tour Eiffel Street",
     cols: 2,
-    rows: 1
+    rows: 2
   }
 ];
 
