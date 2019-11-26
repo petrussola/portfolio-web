c3ab23204cebe5e2719db5ddd19e8067a6c56617
Pere <pere.solaclaver@gmail.com>
2019-11-26 10:30:29 +0000

    Create biolterplate react components

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./App.css";
 import { Route } from "react-router-dom";
 
+// IMPORT COMPONENTS
+import Header from "./components/Header";
+import ContactSection from "./components/ContactSection";
+import HomePage from "./components/HomePage";
+import About from "./components/About";
+import ContactPage from "./components/ContactPage";
+import Project from "./components/Project";
+
 function App() {
   return (
     <div className="App">
       <Route path="/" component={Header} />
-      <Route path="/" component={Contact} />
+      <Route path="/" component={ContactSection} />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={About} />
+      <Route path="/contact" component={ContactPage} />
       <Route path="/projects/:id" component={Project} />
     </div>
   );
---
+++ src/components/ContactSection.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function ContactSection() {
+return (
+<div>
+</div>
+);
+};
