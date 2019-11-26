327bd954220de424ebcabfefb8f52c688768bc3e
Pere <pere.solaclaver@gmail.com>
2019-11-26 11:25:30 +0000

    Link from page title

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 // MATERIAL UI
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   root: {
     background: "yellow",
-    width: '50%'
+    width: "50%"
   }
 });
 
@@ -13,7 +15,9 @@ export default function Title() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <h1>PSC</h1>
+      <Link to="/">
+        <h1>PSC</h1>
+      </Link>
     </div>
   );
 }
