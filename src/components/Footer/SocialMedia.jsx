a0ac48adaa65ead1724b8db155a2326c29408382
Pere <pere.solaclaver@gmail.com>
2019-11-26 13:47:05 +0000

    Add links to social media icons

--- src/components/Footer/SocialMedia.jsx
+++ src/components/Footer/SocialMedia.jsx
@@ -17,15 +17,33 @@ export default function SocialMedia() {
   const classes = useStyles();
   return (
     <div>
-      <IconButton className={classes.icon}>
-        <TwitterIcon fontSize={"large"} />
-      </IconButton>
-      <IconButton className={classes.icon}>
-        <LinkedInIcon fontSize={"large"} />
-      </IconButton>
-      <IconButton className={classes.icon}>
-        <GitHubIcon fontSize={"large"} />
-      </IconButton>
+      <a
+        href="https://twitter.com/petrussola"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton className={classes.icon}>
+          <TwitterIcon fontSize={"large"} />
+        </IconButton>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/peresola/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton className={classes.icon}>
+          <LinkedInIcon fontSize={"large"} />
+        </IconButton>
+      </a>
+      <a
+        href="https://github.com/petrussola"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconButton className={classes.icon}>
+          <GitHubIcon fontSize={"large"} />
+        </IconButton>
+      </a>
     </div>
   );
 }
