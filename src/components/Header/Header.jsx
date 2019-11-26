524c772e768ef3b384a3a2e6f0c566efd9cb02dd
Pere <pere.solaclaver@gmail.com>
2019-11-26 10:34:59 +0000

    Create title and navbar components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./Title";
+import NavBar from "./NavBar";
+
+export default function Header() {
+  return (
+    <div>
+      <Title />
+      <NavBar />
+    </div>
+  );
+}
