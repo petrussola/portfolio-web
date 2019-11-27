684959a193b6855c89a989320cd1bd11ddd0f410
Pere <pere.solaclaver@gmail.com>
2019-11-27 14:10:09 +0000

    Add another project

--- src/database/Projects.js
+++ src/database/Projects.js
@@ -33,35 +33,24 @@ const ProjectData = [
   {
     id: 4,
     img:
-      "https://i.imgur.com/XGFgQWw.png",
+      "https://i.imgur.com/8rISTOo.png",
     title: "Women's World Cup Football search",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac aliquam orci, sed viverra odio. Duis aliquam leo id mollis luctus. Nam id nisl purus. Duis mauris enim, tincidunt id maximus nec, eleifend consectetur enim. Cras scelerisque, ex cursus porta pulvinar, leo turpis sagittis augue, id fermentum nibh nisi sed.",
     url: null,
-    cols: 2,
+    cols: 1,
     rows: 1
   },
   {
     id: 5,
     img:
-      "https://images.unsplash.com/photo-1509041322357-8a3f9757a475?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80",
-    title: "Tour Eiffel Street",
+      "https://i.imgur.com/XvTo2Ma.png",
+    title: "I'll Server Soup - Kitchen Management App - Marketing Page",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac aliquam orci, sed viverra odio. Duis aliquam leo id mollis luctus. Nam id nisl purus. Duis mauris enim, tincidunt id maximus nec, eleifend consectetur enim. Cras scelerisque, ex cursus porta pulvinar, leo turpis sagittis augue, id fermentum nibh nisi sed.",
-    url: "",
-    cols: 1,
-    rows: 2
-  },
-  {
-    id: 6,
-    img:
-      "https://images.unsplash.com/photo-1431274172761-fca41d930114?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-    title: "Tour Eiffel Street",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac aliquam orci, sed viverra odio. Duis aliquam leo id mollis luctus. Nam id nisl purus. Duis mauris enim, tincidunt id maximus nec, eleifend consectetur enim. Cras scelerisque, ex cursus porta pulvinar, leo turpis sagittis augue, id fermentum nibh nisi sed.",
-    url: "",
+    url: null,
     cols: 2,
-    rows: 2
+    rows: 1
   }
 ];
 
