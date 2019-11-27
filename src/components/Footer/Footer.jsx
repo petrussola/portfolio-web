cfea5692bb420fb7fd9cf1cd23d492b0bccf267a
Pere <pere.solaclaver@gmail.com>
2019-11-27 12:43:00 +0000

    Make contact footer dynamic based o page visited

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 // COMPONENTS
 import FormContainer from "./FormContainer";
 import ContactDetails from "./ContactDetails";
+import Copyright from "./Copyright";
 
 // MATERIAL UI
 import { makeStyles } from "@material-ui/core/styles";
@@ -12,7 +13,7 @@ const useStyles = makeStyles({
     background: "black",
     color: "white",
     width: "100%",
-    height: "300px"
+    height: "auto"
   },
   container: {
     width: "50%",
@@ -24,15 +25,24 @@ const useStyles = makeStyles({
   }
 });
 
-export default function Footer() {
+export default function Footer(props) {
   const [inFooter, setInFooter] = useState(true);
   const classes = useStyles();
-  return (
-    <div className={classes.root} id={"test"}>
-      <div className={classes.container}>
-        <ContactDetails />
-        <FormContainer inFooter={inFooter} />
+  debugger;
+  if (props.location.pathname === "/contact") {
+    return (
+      <div className={classes.root} id={"test"}>
+        <Copyright />
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className={classes.root} id={"test"}>
+        <div className={classes.container}>
+          <ContactDetails />
+          <FormContainer inFooter={inFooter} />
+        </div>
+      </div>
+    );
+  }
 }
