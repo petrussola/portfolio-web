1f693c788765eb35fffb6e6adda8d784fde88dad
Pere <pere.solaclaver@gmail.com>
2019-11-28 23:32:38 +0000

    Code to send email when contact form us completed

--- src/components/Footer/FormContainer.jsx
+++ src/components/Footer/FormContainer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import ContactUsForm from "./ContactUsForm";
+import axios from "axios";
 
 // MATERIAL UI
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,6 +21,14 @@ const initialValues = {
 export default function FormContainer({ inFooter }) {
   const submitFormHandler = values => {
     console.log(values);
+    axios
+      .post("http://localhost:4400/contact", values)
+      .then(data => {
+        debugger;
+      })
+      .catch(error => {
+        debugger;
+      });
   };
   const classes = useStyles();
   return (
