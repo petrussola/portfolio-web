17cb5152fda178ec40bcf538ccb05d03006c1040
Pere <pere.solaclaver@gmail.com>
2019-11-28 12:48:08 +0000

    Style contact buttons for social media in contact page

--- src/components/Contact/ContactDetails.jsx
+++ src/components/Contact/ContactDetails.jsx
@@ -2,52 +2,67 @@ import React from "react";
 
 // MATERIAL UI
 import { makeStyles } from "@material-ui/core";
-import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import { Button } from "@material-ui/core";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
-    width: "50%"
+    width: "50%",
+    display: "flex",
+    "flex-direction": "column",
+    "align-items": "center",
+    "justify-content": "center"
   },
   list: {
     width: "50%",
     margin: "0 auto"
+  },
+  button: {
+    width: "50%",
+    margin: theme.spacing(1)
   }
-});
+}));
 
 export default function ContactDetails() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <List className={classes.list}>
-        <ListItemLink href="https://www.linkedin.com/in/peresola/">
-          <ListItemIcon>
-            <LinkedInIcon fontSize={"large"} />
-          </ListItemIcon>
-          <ListItemText primary="LinkedIn" />
-        </ListItemLink>
-        <ListItemLink href="https://twitter.com/petrussola">
-          <ListItemIcon>
-            <TwitterIcon fontSize={"large"} />
-          </ListItemIcon>
-          <ListItemText primary="Twitter" />
-        </ListItemLink>
-        <ListItemLink href="https://github.com/petrussola">
-          <ListItemIcon>
-            <GitHubIcon fontSize={"large"} />
-          </ListItemIcon>
-          <ListItemText primary="GitHub" />
-        </ListItemLink>
-      </List>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        className={classes.button}
+        startIcon={<TwitterIcon />}
+        href="https://twitter.com/petrussola"
+        fullWidth="true"
+      >
+        Twitter
+      </Button>
+      <Button
+        variant="contained"
+        color="inherit"
+        size="large"
+        className={classes.button}
+        startIcon={<LinkedInIcon />}
+        href="https://www.linkedin.com/in/peresola/"
+        fullWidth="true"
+      >
+        Linkedin
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="large"
+        className={classes.button}
+        startIcon={<GitHubIcon />}
+        href="https://github.com/petrussola"
+        fullWidth="true"
+      >
+        GitHub
+      </Button>
     </div>
   );
-}
-
-function ListItemLink(props) {
-  return <ListItem button component="a" selected={false} {...props} />;
 }
